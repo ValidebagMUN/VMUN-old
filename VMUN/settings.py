e32ffc03b0adb314256185da881401183c4c4f2f
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-01-17 23:32:37 +0300

    Add Database config exception for CI

--- VMUN/settings.py
+++ VMUN/settings.py
@@ -99,16 +99,17 @@ WSGI_APPLICATION = 'VMUN.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+if not env("CI"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env("DB_NAME"),
+            'USER': env("DB_USER"),
+            'PASSWORD': env("DB_PASSWORD"),
+            'HOST': env("DB_HOST"),
+            'PORT': env("DB_PORT"),
+        }
     }
-}
 
 
 # Password validation
