f2af89cdce606fe00c4a4be9624a1531b0870f40
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-01-20 15:23:19 +0300

    modified:   VMUN/settings.py

--- VMUN/settings.py
+++ VMUN/settings.py
@@ -11,12 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from django.core.management.utils import get_random_secret_key
 import os
-
 from pathlib import Path
-import environ
-
-env = environ.Env()
-env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
