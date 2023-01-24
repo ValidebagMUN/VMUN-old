4bfb46ee3e11c1fe7d0ecc44e3ebdafbed9a4306
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-01-24 17:37:05 +0300

    Add Caucus URL&view config

--- caucus/models.py
+++ caucus/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from participants.models import Delegate
+from committee.models import Committee
 # Create your models here.
+
+
+class Caucus(models.Model):
+    TYPES = [
+        ('MOD', 'Moderated Caucus'),
+        ('UNM', 'Un-moderated Caucus'),
+        ('SMM', 'Semi-Moderated Caucus'),
+    ]
+    type = models.CharField(max_length=3, choices=TYPES, default='MOD', verbose_name='Caucus Type')
+    topic = models.CharField(max_length=100, verbose_name='Caucus Topic')
+    description = models.TextField(verbose_name='Caucus Description', blank=True, null=True)
+    start_time = models.DateTimeField(verbose_name='Caucus Start Time')
+    duration = models.DurationField(verbose_name='Caucus Duration')
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE, verbose_name='Committee')
+    proposer = models.ForeignKey(Delegate, on_delete=models.CASCADE, verbose_name='Proposer of the Caucus')
+    participants = models.ManyToManyField(Delegate, verbose_name='Caucus Participants', blank=True,
+                                          related_name='caucus_participants')
+
+    def __str__(self):
+        return self.topic
+
+    class Meta:
+        verbose_name = 'caucus'
+        verbose_name_plural = 'caucuses'
--- caucus/views.py
+++ caucus/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
-
+from django.conf.urls import handler404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Caucus
 # Create your views here.
+
+
+@login_required(login_url='/login/')
+def mod_view(request, *args, **kwargs):
+    caucus = Caucus.objects.get(id=id)
+    if caucus.type != 'MOD':
+        if caucus.type == 'UNM':
+            return redirect('caucus:unmod', id=id)
+        elif caucus.type == 'SMM':
+            return redirect('caucus:semimod', id=id)
+    else:
+        exception = 'Caucus Type Error'
+        handler404(request, exception)
+    context = {
+        'id': id,
+    }
+    return render(request, 'mod.html', context)
+
+
+@login_required(login_url='/login/')
+def unmod_view(request, *args, **kwargs):
+    caucus = Caucus.objects.get(id=id)
+    if caucus.type != 'UNM':
+        if caucus.type == 'MOD':
+            return redirect('caucus:mod', id=id)
+        elif caucus.type == 'SMM':
+            return redirect('caucus:semimod', id=id)
+    else:
+        exception = 'Caucus Type Error'
+        handler404(request, exception)
+    context = {
+
+    }
+    return render(request, 'unmod.html', context)
+
+
+@login_required(login_url='/login/')
+def semimod_view(request, *args, **kwargs):
+    caucus = Caucus.objects.get(id=id)
+    if caucus.type != 'SMM':
+        if caucus.type == 'MOD':
+            return redirect('caucus:mod', id=id)
+        elif caucus.type == 'UNM':
+            return redirect('caucus:unmod', id=id)
+    else:
+        exception = 'Caucus Type Error'
+        handler404(request, exception)
+    context = {
+        'id': id,
+    }
+    return render(request, 'semimod.html', context)
+
+
+@login_required(login_url='/login/')
+def create_view(request, *args, **kwargs):
+    context = {
+
+    }
+    return render(request, 'create.html', context)
